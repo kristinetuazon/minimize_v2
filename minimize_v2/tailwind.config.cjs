74564a5ff7994322823cd8838724b430cf94a0b9
kristinetuazon <kristine.jane.tuazon@gmail.com>
2023-01-30 12:40:05 +0900

    feat: inital set up finished start making button component

--- minimize_v2/tailwind.config.cjs
+++ minimize_v2/tailwind.config.cjs
@@ -2,6 +2,28 @@
 module.exports = {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
+    screens: {
+      sm: '400px',
+      md: '768px',
+      lg: '976px',
+      xl: '1440px',
+    },
+    colors: {
+      blackish: "#2D2D2B",
+      powder: "#F0E4D8",
+      offWhite: "#F6F5F0",
+      supple: "#C39E9E",
+      fadedPink: "#F6F0F1"
+    },
+    fontFamily: {
+      title: ['Holipop','serif'],
+      heading: ['Bargemo', 'serif'],
+      bodyLight: ['Petrona_Light', 'serif'],
+      bodyRegular: ['Petrona_Regular', 'serif'],
+      bodyThin: ['Petrona_Thin', 'serif'],
+      bodyRegularItalic: ['Petrona_Italic', 'serif'],
+      bodyThinItalic: ['Petrona_Thin_Italic', 'serif'],
+    },
     extend: {},
   },
   plugins: [],
